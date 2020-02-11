4325e8061a63750968d3730ecbdaa493a907f868
Arthur Barbosa <arthurb@ciandt.com>
2020-02-11 14:54:47 -0300

    Bug fixes and file name improvement

--- initial_config.sh
+++ initial_config.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
+## install homebrew
+
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
+## install zsh
 
 brew install zsh zsh-completions
 
@@ -10,16 +17,32 @@ HISTFILE="${HOME}/.zsh_history"
 HISTSIZE=2000
 SAVEHIST=2000
 
+
+## install z
+
 brew install z
 
 cat echo ". ~/z.sh" >> ~/.zshrc
+
+## install fzf
 
 brew install fzf
 
 cat echo "[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh" >> ~/.zshrc
 
-#https://www.smashingmagazine.com/2015/07/become-command-line-power-user-oh-my-zsh-z/
 
+##links úteis
+
+#https://www.smashingmagazine.com/2015/07/become-command-line-power-user-oh-my-zsh-z/
 #https://github.com/junegunn/fzf
 
+
+## install python
+
 brew install python3
+
+
+## install mongo
+
+brew install mongodb-community@4.2
+
